55f06e3a232c63f461a34431570b177176ac064b
Lee dohyeong <alsgudwpxm31@gmail.com>
2022-03-22 13:30:03 +0900

    12121212

--- src/dddddd.java
+++ src/dddddd.java
@@ -3,9 +3,24 @@ public class dddddd {
 
 	public static void main(String[] args) {
 		// ddddddddd
+		
+		boolean b = false;
+		
+		char a = 'a';
+		
+		b = (a == 'a')? true : false;
+		
+		System.out.println("b : " + b);
+		
+		
+		
+		
+		
+		
 		System.out.println("dddddd");
 		System.out.println("dddddd");
 		System.out.println("dddddd");//sss
+		System.out.println("dddddd121221");//sss
 		System.out.println("dddddd121221");//sss
 	}
 
